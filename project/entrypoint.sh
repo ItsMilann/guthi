69d448e883c0dad319240409a42fc60628d8f303
itsmilann <itsmilann@gitlab.com>
2024-10-04 12:57:24 +0545

    chore: remove fixtures and update script

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -4,8 +4,6 @@ deploy_hard(){
 	echo "Hard Deploy Selected"
 	python manage.py collectstatic
 	python manage.py migrate
-	python manage.py loaddata seed.json
-
 	uwsgi --ini uwsgi.ini
 }
 
@@ -14,7 +12,6 @@ deploy_soft(){
 	echo "Soft deploy Selected"
 	python manage.py collectstatic
 	python manage.py migrate
-	python manage.py loaddata fixtures/users.json
 
 	uwsgi --ini uwsgi.ini --py-autoreload 1
 }
